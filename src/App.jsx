6d6bb640992d2fc54e3d42e403e520382f3a1264
CodeSandbox <support@codesandbox.io>
2023-05-18 05:40:31 +0000

    "Initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Adopt Me!</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const Pet = () => {
+  return (
+    <div>
+      <h1>Arya</h1>
+    </div>
+  );
+};
